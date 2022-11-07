0bd8b51a443d5eaa00621d1689477c369f69ad98
Dominic <dominic.kirste@gmail.com>
2022-11-07 14:28:31 +0100

    Data and Methods - created component IncrementButton - with data function with count set to 0 - with method increment - added component to HomeView

---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="increment()">You clicked me {{ count }} times</button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IncrementButton",
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment: function () {
+      this.count++;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <IncrementButton />
     <HelloWorld msg="Welcome to Home" />
   </div>
 </template>
@@ -7,11 +8,13 @@
 <script lang="ts">
 import Vue from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import IncrementButton from "@/components/IncrementButton.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "HomeView",
   components: {
     HelloWorld,
+    IncrementButton,
   },
 });
 </script>
